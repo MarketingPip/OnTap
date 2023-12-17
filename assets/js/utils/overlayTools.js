292bebb5262538b9d6ed0e318f670d08a27e2cc8
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2023-12-17 17:08:22 -0500

    Create overlayTools.js

---
+++ assets/js/utils/overlayTools.js
@@ -0,0 +1,18 @@
+export function hideOverlay(){
+       const body = document.body;
+
+
+  body.classList.add('modal-open');
+   
+      //
+  }
+  
+  
+export function showOverlay(){
+     const body = document.body;
+
+    
+     body.classList.remove('modal-open');
+     // mainScreen.classList.remove('modal-open');
+}
+
