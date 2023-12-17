ee7e7dac82ba0d1c2963faec4125be70d20839cc
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2023-12-17 15:03:38 -0500

    Update updateFetcher.js

--- assets/js/updateFetcher.js
+++ assets/js/updateFetcher.js
@@ -1,3 +1,9 @@
+function updateCheck(){
+
+function isOnline() {
+  return navigator ? navigator.onLine : false;
+}
+	
 async function getLatestRelease(username, repo) {
   try {
     const apiUrl = `https://api.github.com/repos/${username}/${repo}/releases/latest`;
@@ -17,19 +23,13 @@ async function getLatestRelease(username, repo) {
       url: data.html_url,
     };
   } catch (error) {
-    console.error("Error:", error.message);
-    // You can handle the error further or throw it to let the calling code handle it
     throw error;
   }
 }
 
-function isOnline() {
-  return navigator ? navigator.onLine : false;
-}
-
 async function getLatestReleaseWithInternetCheck(username, repo) {
   try {
-    const online = await isOnline();
+    const online = isOnline();
 
     if (!online) {
       throw new Error("No internet connection");
@@ -38,26 +38,51 @@ async function getLatestReleaseWithInternetCheck(username, repo) {
     const latestRelease = await getLatestRelease(username, repo);
     return latestRelease;
   } catch (error) {
-    console.error("Error:", error.message);
+   // console.error("Error:", error.message);
     // You can handle the error further or throw it to let the calling code handle it
     throw error;
   }
 }
-
-
+	
 const username = 'MarketingPipeline';
 const repo = 'Termino.js';
 
-let currentVersion = 1
+
 
 getLatestReleaseWithInternetCheck(username, repo)
   .then((latestRelease) => {
     const latest_version = latestRelease.tagName.replace("v", "")
-    if(latest_version != "1.0.0"){
-      console.log(`New version available! 🚀 It appears you are using ${currentVersion}, feel free to update now to version: ${latest_version}. Released on ${latestRelease.releaseDate.split("T")[0]}`)
+    if(latest_version != version){
+    showUpdateModal(latestRelease, latest_version)	  
     }
-    console.log("Latest Release:", latestRelease);
+    //console.log("Latest Release:", latestRelease);
   })
   .catch((error) => {
     console.error("Error:", error.message);
   });
+
+
+}
+
+
+function showUpdateModal(latestRelease, latest_version){
+
+openModal('New version available! 🚀', `
+    <p class="mb-4">
+    It appears you are using ${version}, feel free to update now to version: ${latest_version}. Released on ${latestRelease.releaseDate.split("T")[0]}.  
+    </p>
+ <!-- Save and Cancel buttons -->
+<div class="flex flex-col md:flex-row justify-end space-y-4 md:space-y-0 md:space-x-4">
+    <a href="${latestRelease.url}" class="maia-button maia-button-primary">
+        <button class="w-full md:w-auto inline-flex items-center justify-center font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 text-white text-lg md:text-xl p-2 md:p-4 rounded-lg bg-green-500">
+           View Latest Release
+        </button>
+    </a>
+    <button onclick="hideModal()" class="w-full md:w-auto inline-flex items-center justify-center font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-gray-300 h-10 text-white text-lg md:text-xl p-2 md:p-4 rounded-lg bg-gray-500">
+        No, I'll update later
+    </button>
+</div>
+
+
+`, 'You can report an issue on GitHub by clicking');
+}
