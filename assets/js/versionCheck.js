3a440878c823b279126638daada72353d2c6c424
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2023-12-17 15:06:58 -0500

    Create versionCheck.js

---
+++ assets/js/versionCheck.js
@@ -0,0 +1,16 @@
+function validateVersion(version) {
+    // Split the version string using dot as a delimiter
+    const versionNumbers = version.split('.');
+
+    // Extract the first number from the version string
+    const firstNumber = parseInt(versionNumbers[0]);
+
+    // Check if the first number is 1
+    if (firstNumber === 1) {
+        return {error:false}
+    } else if (firstNumber > 1) {
+        return {error:'Menu Version is too high. You need to upgrade your software to open this menu.'}
+    } else {
+        return {error:'Menu Version is too low. You need to downgrade your software to open this menu.'}
+    }
+}
