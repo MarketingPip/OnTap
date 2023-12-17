816cc13080feaf71eb4730502ed91b4b88ab0887
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2023-12-17 15:29:49 -0500

    Update pageLoaderRemover.js

--- assets/js/pageLoaderRemover.js
+++ assets/js/pageLoaderRemover.js
@@ -1,5 +1,5 @@
    // Function to remove the loader and show content
-        function removeLoader() {
+        function removeLoader(isHomePage = false) {
             document.getElementById('page-loader').classList.add('opacity-0');
             setTimeout(function () {
               document.getElementById('page-loader').style.opacity = 0 
@@ -7,6 +7,10 @@
               
           
              hidePageLoader()
+
+             if(isHomePage){
+               updateCheck()
+             }
 
             }, 3150); // completes animations at this time 
           
